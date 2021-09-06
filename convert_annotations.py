0a0fc2cb7780f55ab59904dd35c8cc947433a174
fdloopes <fdloopes@gmail.com>
2021-09-06 02:42:49 -0300

    Criado script para ler/criar as mascaras através dos arquivos xml

---
+++ convert_annotations.py
@@ -0,0 +1,63 @@
+import xml.etree.ElementTree as ET
+import numpy as np
+import cv2
+import glob
+
+'''
+map colors
+
+tooth = white
+restoration = blue
+crown = yellow
+dental implant = green
+root canal treatment = purple
+pulp = red
+'''
+def main():
+    # get all paths to xmls in path
+    files = glob.glob("Annotations/*.xml")
+    points = []
+    for file in files:
+        tree = ET.parse(file)
+        root = tree.getroot()
+        # read image to get shape
+        image = cv2.imread(file.replace("Annotations/","Images/").replace("xml","jpg"))
+        # create mask image with zeros
+        mask = np.zeros((image.shape))
+        # loop to all objects in xml file
+        for neighbor in root.findall("object"):
+            polygon = neighbor.find('polygon')
+            # get points
+            for pt in polygon.findall('pt'):
+                x = int(round(float(pt.find("x").text)))
+                y = int(round(float(pt.find("y").text)))
+                points.append([x,y])
+
+            # Verify which one polygon is to set color
+            if neighbor.find('name').text == "restoration":
+                color = (255,0,0)
+                mask = cv2.fillPoly(mask,np.int32([points]),color)
+            if neighbor.find('name').text == "tooth":
+                color = (255,255,255)
+                mask = cv2.drawContours(mask, np.int32([points]), -1, color, 3)
+            if neighbor.find('name').text == "crown":
+                color = (0,255,255)
+                mask = cv2.fillPoly(mask,np.int32([points]),color)
+            if neighbor.find('name').text == "dental implant":
+                color = (0,128,0)
+                mask = cv2.fillPoly(mask,np.int32([points]),color)
+            if neighbor.find('name').text == "root canal treatment":
+                color = (128,0,128)
+                mask = cv2.fillPoly(mask,np.int32([points]),color)
+            if neighbor.find('name').text == "pulp":
+                color = (0,0,255)
+                mask = cv2.fillPoly(mask,np.int32([points]),color)
+            
+            points = []
+        
+        cv2.imwrite(file.replace("Annotations/","Masks/").replace("xml","png"),mask)
+        print("Create Mask from file: ",file)
+        mask[:] = 0
+
+if __name__ == '__main__':
+    main()
