592dba2d259b4699d414f266ee755e611f783e8d
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2022-09-16 14:49:34 -0300

    Modificando o xmltojson.py para gerar json que possam ser usados no detectron2

--- xmltojson.py
+++ xmltojson.py
@@ -4,7 +4,22 @@ import glob
 import os
 import sys
 
-def main(xml_path, json_path):
+def parse_folder_to_file(xml_path, json_path):
+	files = glob.glob(os.path.join(xml_path, '*.xml'))
+	annot_dict = {}
+
+	for file_ in files:
+		file_name = file_.split('/')[-1].replace('.xml', '')
+
+		with open(file_, 'r') as reader:
+			annot_dict.update({f"{file_name}" : xmltodict.parse(reader.read())})
+
+		with open(json_path, 'w') as writer:
+			writer.write(json.dumps(annot_dict))
+
+		#print(f"Parsed xml to json of {file_name}")
+
+def parse_folder_to_folder(xml_path, json_path):
 	files = glob.glob(os.path.join(xml_path, '*.xml'))
 
 	if not os.path.exists(json_path):
@@ -12,7 +27,7 @@ def main(xml_path, json_path):
 
 	for file_ in files:
 		file_json = os.path.join(json_path, file_.split('/')[-1].replace('xml', 'json'))
-		annot_dict = ''
+		annot_dict = {}
 
 		with open(file_, 'r') as reader:
 			annot_dict = xmltodict.parse(reader.read())
@@ -21,4 +36,4 @@ def main(xml_path, json_path):
 			writer.write(json.dumps(annot_dict))
 
 if __name__ == '__main__':
-	main(sys.argv[1], sys.argv[2])
+	parse_folder_to_file(sys.argv[1], sys.argv[2])
